74edf5befa1596f787d584d60ade623ea40ce9c9
Mohana Vamsi <mohana.gangisetti@kalvium.community>
2024-03-03 12:26:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PUBLIC_PORT;
+
 app.get("/",(req,res)=>{
     res.send("hey go to ping")
 })
 app.get("/ping",(req,res)=>{
   res.send("pong")
 })
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`server running on PORT: ${3000}`);
   });
 
