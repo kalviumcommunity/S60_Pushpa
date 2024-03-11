435d57f5e3976f9185bec3e1d1dd2ea8ca5eed8d
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-03-11 10:20:57 +0530

    added connection status:

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const app = express();
+const {connectDB,isConnected}=require("./db")
+require("dotenv").config()
 const port = 5000;
-
+connectDB(process.env.DB_URI)
 app.get("/",(req,res)=>{
-    // res.send("hey go to ping")
-    res.redirect(req.baseUrl+"/ping")
-})
-app.get("/ping",(req,res)=>{
-  res.send("pong")
+  res.send(isConnected()? '<h1>Connected to db</h1>':'<h1>Not connected yet!</h1>')
+
 })
 app.listen(port, () => {
     console.log(`server running on PORT: ${port}`);
-  });
-
-
+});
 module.exports = app;
