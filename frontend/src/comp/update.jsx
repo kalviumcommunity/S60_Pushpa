d46c9f9e96a167c708c21229c34f9c1f0812be20
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-04-05 10:38:02 +0530

    added

--- frontend/src/comp/update.jsx
+++ frontend/src/comp/update.jsx
@@ -34,7 +34,7 @@ function Update() {
        placeholder="Name"/>
        
        <input className="bg-slate-900 text-white w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-gray-800" 
-       name="age"
+       name="Age"
        onChange={(e)=>{va(e)}} value={data.age}
        placeholder="Age"/>
        
